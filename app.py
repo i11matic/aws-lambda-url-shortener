3916b805114e590113903f882cbbd156c62b10a1
Mark Agustin <magustin0329@gmail.com>
2023-02-19 23:37:18 -0800

    printing event data

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def get_original_url(short_url, dynamodb, table_name):
 
 
 def lambda_handler(event, context):
+    print(event)
     if table_name not in dynamodb_client.list_tables()["TableNames"]:
         create_table(dynamodb, table_name)
 
